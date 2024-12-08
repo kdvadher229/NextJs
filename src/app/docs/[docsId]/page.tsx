92d1784defd91ba6e56b9bc787b9a4405e3a0bd1
khushal vadher aubie <khushal@auberginesolutions.com>
2024-12-08 22:31:27 +0530

    metadata

--- src/app/docs/[docsId]/page.tsx
+++ src/app/docs/[docsId]/page.tsx
@@ -1,4 +1,15 @@
+import {Metadata} from "next"
+type Props ={
+  params:{
+    docsId:string
+  }
+}
 
+export const generateMetadata = ({params}:Props):Metadata=>{
+  return {
+    title:`docs ${params.docsId}`
+  }
+}
 export default function PageId() {
   return (
     <div>page Id</div>
